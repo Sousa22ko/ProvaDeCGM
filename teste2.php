a81152126147032d5bf2cc7bb2b1f12e79e37e73
Reinaldo de sousa carvalho <manomacarraum@gmail.com>
2017-06-14 19:12:24 +0000

    adicionando arquivos novos

---
+++ teste2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang>
+    
+<head>
+
+    <title>teste de build do trevis ci...</title>
+    
+</head>
+
+<body>
+	
+	
+	<?php 
+		echo "Testando a build do travis em um arquivo diferente"	
+		
+	?>
+
+</body>
+
+</html>
