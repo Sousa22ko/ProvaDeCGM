f01f6945523c9c629daba8e614218a2e45b2e772
Reinaldo de sousa carvalho <manomacarraum@gmail.com>
2017-06-14 18:50:50 +0000

    adicionado arquivo yml

---
+++ hello-world.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!-- HTML5 Hello world by kirupa - http://www.kirupa.com/html5/getting_your_feet_wet_html5_pg1.htm -->
+<html lang="pt-br">
+
+<head>
+<title>Hello...</title>
+</head>
+
+<body>
+	
+	<div id="mainContent">
+		<p id="helloText">?</p>
+		<button id="clickButton">click me</button>
+	</div>
+
+</body>
+</html>
